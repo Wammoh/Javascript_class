033bf1e95ee611775bae25a91474afc9644bf076
Troy <troyjohnson.taj@gmail.com>
2022-07-23 23:33:32 -0500

    seats in loops

--- javascript class.js
+++ javascript class.js
@@ -1,49 +1,68 @@
 // x =0
 // while  (x< 100){
-// x+=1
-// console.log(x+" misissippi")
+// // x+=1
+// // console.log(x+" misissippi")
+// // }
+
+
+// // var num = 99
+
+// // while ( num > 0){
+// //     if (num ===1){
+// //     console.log(num + " bottle of juice on the wall! "+num+" bottle of juice! Take one down, pass it around..."+ (num-1) +" bottles of juice on the wall!")
+// //     num -=1}
+// //     else if (num > 2 ){
+// //     console.log(num + " bottles of juice on the wall! "+num+ " bottles of juice! Take one down, pass it around..."+ (num-1) + " bottles of juice on the wall!")
+// //     num -=1}
+// //     else if( num ===2){
+// //     console.log(num+ " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around..."+ (num-1) + " bottle of juice on the wall!")
+// //     num-=1
+// //     }
+// // }
+
+
+// // rocketship
+
+// countdown = 60
+// while (countdown >=0){
+//     if (countdown === 50){
+//         console.log("Orbiter transfers from ground to internal power")
+//         countdown-=1}
+//     else if(countdown ===31){
+//         console.log("Ground launch sequencer is go for auto sequence start")
+//         countdown-=1}
+//     else if(countdown ===16){
+//         console.log("Activate launch pad sound suppression system")
+//         countdown -=1}
+//     else if(countdown ===10){
+//         console.log("Activate main engine hydrogen burnoff system")
+//         countdown-=1}
+//     else if(countdown ===6){
+//         console.log("Main engine start")
+//         countdown-=1}
+//     else if(countdown === 0){
+//         console.log("Solid rocket booster ignition and liftoff! ")
+//         countdown-=1}
+//     else if(countdown >0){
+//     console.log("T-"+countdown+" seconds\n")
+//     countdown-=1}
 // }
 
+// for (x=9; x>=1; x--){
+// //     console.log("hello"+x);
+// // }
 
-// var num = 99
+// let solution = 1;
 
-// while ( num > 0){
-//     if (num ===1){
-//     console.log(num + " bottle of juice on the wall! "+num+" bottle of juice! Take one down, pass it around..."+ (num-1) +" bottles of juice on the wall!")
-//     num -=1}
-//     else if (num > 2 ){
-//     console.log(num + " bottles of juice on the wall! "+num+ " bottles of juice! Take one down, pass it around..."+ (num-1) + " bottles of juice on the wall!")
-//     num -=1}
-//     else if( num ===2){
-//     console.log(num+ " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around..."+ (num-1) + " bottle of juice on the wall!")
-//     num-=1
-//     }
+// for (let i = 1; i <= 12; i++) {
+//   solution *= i;
 // }
 
+// console.log(solution);
 
-// rocketship
 
-countdown = 60
-while (countdown >=0){
-    if (countdown === 50){
-        console.log("Orbiter transfers from ground to internal power")
-        countdown-=1}
-    else if(countdown ===31){
-        console.log("Ground launch sequencer is go for auto sequence start")
-        countdown-=1}
-    else if(countdown ===16){
-        console.log("Activate launch pad sound suppression system")
-        countdown -=1}
-    else if(countdown ===10){
-        console.log("Activate main engine hydrogen burnoff system")
-        countdown-=1}
-    else if(countdown ===6){
-        console.log("Main engine start")
-        countdown-=1}
-    else if(countdown === 0){
-        console.log("Solid rocket booster ignition and liftoff! ")
-        countdown-=1}
-    else if(countdown >0){
-    console.log("T-"+countdown+" seconds\n")
-    countdown-=1}
+for (var seat = 100; seat < 1; seat--){
+    for(let row = 25; row <1; row--){
+        console.log(seat +"-"+ row)
+    }
 }
