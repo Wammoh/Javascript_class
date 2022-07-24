f658f704b40b623dabcfb292c48194cb568f0289
Troy <troyjohnson.taj@gmail.com>
2022-07-23 19:24:28 -0500

    working on bottle loop

--- javascript class.js
+++ javascript class.js
@@ -1,5 +1,13 @@
-x =0
-while  (x< 100){
-x+=1
-console.log(x+" misissippi")
-}
+// x =0
+// while  (x< 100){
+// x+=1
+// console.log(x+" misissippi")
+// }
+
+
+var num = 5
+
+while ( num >= 2){
+        console.log(num + "bottles of juice on the wall!"+num+ "bottles of juice! Take one down, pass it around..."+ num + " bottles of juice on the wall!")
+        num-=1
+    }
